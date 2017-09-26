28db618ac0ddc02b0c949d0f65206ba1bfd0b288
HelloBetafish <bethany.pfeister@gmail.com>
2017-09-26 12:45:21 -0700

    checked formatting

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 var Twitter = require('twitter');
-var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var keys = require('./keys.js');
 var fs = require('fs');
 
 var client = new Twitter({
@@ -16,47 +16,51 @@ var spotify = new Spotify({
   secret: keys.spotifyKeys.secret
 });
 
+var userCommand = process.argv[2];
 var screenName = 'HelloBetafish';
-var movieName = "";
 var songName = "";
 var artists = "";
-var userCommand = process.argv[2];
+var movieName = "";
 
 switch(userCommand) {
-  case "my-tweets" :
+  case "my-tweets":
   tweets();
   break;
 
-  case "spotify-this-song" :
+  case "spotify-this-song":
   spotifySong();
   break;
 
-  case "movie-this" :
+  case "movie-this":
   movie();
   break;
 
-  case "do-what-it-says"  :
+  case "do-what-it-says":
   random();
   break;
 }
 
 function tweets() {
 // Allow user to put in any screenname to pull up another user's tweets.
-  if (process.argv[3]){
+  if (process.argv[3]) {
   screenName = process.argv[3];
   }
 
   var params = {screen_name: screenName};
+
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
   	if (!error) {
-  	  for (var i = 0 ; i < tweets.length ; i++){
+  	  for (var i = 0 ; i < tweets.length ; i++) {
+  	  	// Limit to 20 Tweets
   	  	if (i === 20) { break;}
+
   	  	var counter = i + 1;
+  	  	// Output Tweets to console
   	  	console.log("--------------------------");
   	  	console.log("Tweet #" + counter + ":");
   	    console.log(tweets[i].text);
   	    console.log("Time Created: " + tweets[i].created_at);
-  	    
+  	    // Write to log.txt
   	    fs.appendFile("log.txt", "--------------------------\r\n" + "Tweet #" + counter + ":" +
   	      tweets[i].text + "\r\nTime Created: " + tweets[i].created_at + "\r\n", function(error) {
   	      if (error){
@@ -73,7 +77,7 @@ function tweets() {
 }
 
 function spotifySong() {
-
+  // Default song if no input
   if (!process.argv[3]) {
   	songName = "We Are Never Ever Getting Back Together";
   }
@@ -83,28 +87,29 @@ function spotifySong() {
   	  songName += process.argv[i] + " ";
     }
   }
+
   spotify.search({type: 'track', query: songName, limit: 1}, function(error, data) {
+
   	if (error) {
   	  return console.log('Error occured: ' + error);
   	}
-  	// console.log(data);
-  	// console.log(data.tracks.items);
-  	for (var i = 0 ; i < data.tracks.items[0].artists.length ; i++){
+
+  	for (var i = 0 ; i < data.tracks.items[0].artists.length ; i++) {
   	  artists += data.tracks.items[0].artists[i].name + " ; ";
   	}
-
+  	// Output song info to console
   	console.log("--------------------------");
   	console.log("Artist(s): " + artists);
   	console.log("Name of Song: " + songName);
   	console.log("Preview URL: " + data.tracks.items[0].preview_url);
   	console.log("Album: " + data.tracks.items[0].album.name);
   	console.log("--------------------------");
-
+  	// Write to log.txt
   	fs.appendFile("log.txt", "--------------------------\r\n" + "Artist(s): " + artists + "\r\nName of Song: " + 
   	  songName + "\r\nPreview URL: " + data.tracks.items[0].preview_url + "\r\nAlbum: " + 
   	  data.tracks.items[0].album.name + "\r\n", function(error) {
 
-  	  if (error){
+  	  if (error) {
   	    return console.log(error);
 	  }	
   	});
@@ -112,30 +117,33 @@ function spotifySong() {
 }
 
 function movie() {
-
-  if (!process.argv[3]){
+  // Default movie if no input
+  if (!process.argv[3]) {
   	movieName = "Mr. Nobody";
   }
 
-  else{
-  	for (var i = 3 ; i < process.argv.length ; i++){
+  else {
+    for (var i = 3 ; i < process.argv.length ; i++){
   	  movieName += process.argv[i] + " ";
   	}
   }
 
   var queryURL = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=40e9cece';
+
   request(queryURL, function(error, response, body) {
-  	if(!error && response.statusCode === 200){
+
+  	if(!error && response.statusCode === 200) {
   	  // Look for Rotten Tomatoes Rating if it exists
   	  var RTrating = "";
-  	  if (JSON.parse(body).Ratings[1] != undefined){
+
+  	  if (JSON.parse(body).Ratings[1] != undefined) {
   	  	RTrating = JSON.parse(body).Ratings[1].Value;
   	  }
-  	  else{
+
+  	  else {
   	  	RTrating = "undefined";
   	  }
-  	  // Output info about movie
-  	  // console.log(body);
+  	  // Output movie info to console
   	  console.log("--------------------------");
   	  console.log("Title: " + JSON.parse(body).Title);
   	  console.log("Year: " + JSON.parse(body).Year);
@@ -146,18 +154,19 @@ function movie() {
   	  console.log("Plot: " + JSON.parse(body).Plot);
   	  console.log("Actors: " + JSON.parse(body).Actors);
   	  console.log("--------------------------");
-
+  	  // Write to log.txt
   	  fs.appendFile("log.txt", "--------------------------\r\n" + "Title: " + JSON.parse(body).Title + "\r\nYear: " + 
   	  	JSON.parse(body).Year + "\r\nIMDB Rating : " + JSON.parse(body).imdbRating + "\r\nRotten Tomatoes Rating: " + 
   	    RTrating + "\r\nCountry: " + JSON.parse(body).Country + "\r\nLanguage: " + JSON.parse(body).Language + 
   	    "\r\nPlot: " + JSON.parse(body).Plot + "\r\nActors: " + JSON.parse(body).Actors + "\r\n", function(error) {
 
-  	    if (error){
+  	    if (error) {
   	      return console.log(error);
 	    }	
   	  });
   	}
-  	else{
+
+  	else {
   	  console.log(error);
   	}
   });
@@ -166,21 +175,24 @@ function movie() {
 function random() {
 
   fs.readFile("random.txt","utf-8", function(error, data) {
-  	if(error) {
+
+  	if (error) {
   	  console.log(error);
   	}
+
   	var dataArray = data.split(",");
   	userCommand = dataArray[0];
   	process.argv[3] = dataArray[1];
+
   	if (userCommand === "my-tweets") {
   	  tweets();
   	}
 
-  	else if (userCommand === "spotify-this-song"){
+  	else if (userCommand === "spotify-this-song") {
   	  spotifySong();
   	}
 
-  	else if (userCommand === "movie-this"){
+  	else if (userCommand === "movie-this") {
   	  movie();
   	}
   });
